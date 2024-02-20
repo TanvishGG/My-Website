4f845e383b2fe7fe57b61e9de40dbcf6464ab7e6
TanvishGG <101194211+TanvishGG@users.noreply.github.com>
2024-02-20 22:24:54 +0000

    F

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 for (let i = 0; i < 8; i++) {
   setTimeout(() => {
     document.getElementById(`${i}`).style.display = "block"
-  }, i * 1500 || 500);
+  }, i * 1300 || 500);
+}
+function boom() {
+  for (let i = 0; i <14; i++) {
+setTimeout(() => { document.getElementById(`${i}`).style.display = "ruby-text"}, i*150 || 100);
+  }
 }
