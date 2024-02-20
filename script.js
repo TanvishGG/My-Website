b33bf1bf54adf2d1aaeaf015f6471407623ae428
TanvishGG <101194211+TanvishGG@users.noreply.github.com>
2024-02-20 22:25:38 +0000

    F

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ for (let i = 0; i < 8; i++) {
   }, i * 1300 || 500);
 }
 function boom() {
-  for (let i = 0; i <14; i++) {
-setTimeout(() => { document.getElementById(`${i}`).style.display = "ruby-text"}, i*150 || 100);
+  for (let i = 0; i < 14; i++) {
+    setTimeout(() => { document.getElementById(`${i}`).style.display = "ruby-text" }, i * 150 || 100);
   }
 }
