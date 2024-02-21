da5e4240f6db5da675b1e6be4ecb5bf5ac1b59ba
TanvishGG <101194211+TanvishGG@users.noreply.github.com>
2024-02-21 07:00:52 +0000

    .

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ for (let i = 0; i < 9; i++) {
     document.getElementById(`${i}`).style.display = "block"
   }, i * 1300 || 500);
 };
-const colors = ["white", "black", "red", "blue", "green", "yellow", "orange"];
+const colors = ["white","red", "blue", "green", "yellow", "orange","skyblue","lime","pink"];
 function boom() {
   for (let i = 0; i < 15; i++) {
     setTimeout(() => {
@@ -17,9 +17,9 @@ function boom() {
       doc.style.letterSpacing = "0.5em";
       doc.style.color = colors[Math.floor(Math.random() * colors.length)];
       doc.style.textShadow = "0 0 0.5em white";
-      doc.style.transition = "all 0.3s ease";
-      doc.style.top = `${Math.random() * 70}%`;
-      doc.style.left = `${Math.random() * 20}%`;
+      doc.style.top = `${Math.random() * 80}%`;
+      doc.style.left = `${Math.random() * 10}%`;
+      doc.style.transition = "all 2s ease";
     }, i * 150 || 100);
   };
 };
