d92b8026ac56283d7e5c973c01c07133e44b4531
Tanvish <101194211+TanvishGG@users.noreply.github.com>
2024-02-23 09:31:26 +0000

    F

--- script.js
+++ script.js
@@ -7,10 +7,8 @@ for (let i = 0; i < 9; i++) {
 const colors = ["white", "red", "blue", "green", "yellow", "orange", "skyblue", "lime", "pink"];
 function boom() {
   for (let i = 0; i <= 17; i++) {
- let doc = document.getElementById(`${i}`)
-  doc.style.animation = "wiggle 1s"
-  doc.style[`animation-iteration-count`] = "infinite";
-    setTimeout( () => { 
+ setTimeout( () => {
+   let doc = document.getElementById(`${i}`)
       doc.textContent = doc.textContent.split("").sort((a, b) => 0.5 - Math.random()).join(" ");
       doc.style.display = "ruby-text";
       doc.style.position = "fixed";
@@ -22,11 +20,12 @@ function boom() {
       doc.style.letterSpacing = "0.5em";
       doc.style.color = colors[Math.floor(Math.random() * colors.length)];
       doc.style.textShadow = "0 0 0.5em white";
-      doc.style.top = `${Math.random() * 100}%`;
+      doc.style.top = `${Math.random() * 80}%`;
       doc.style.left = `${Math.random() * 10}%`;
+      doc.style.transform = `rotateZ(${Math.floor(Math.random()*360)}deg)`;
       doc.style.transition = "all 0.4s ease";
-      doc.style.animation = "none"
-    }, i * 20 + 2300);
+      doc.style.animation = "none";
+    }, i * 20 ?? 10 );
  }
-  setTimeout(() => { window.alert("ooops, i didn't mean to make the nuke button public!\nsorry :(") }, 4000)
+  setTimeout(() => { window.alert("ooops, i didn't mean to make the nuke button public!\nsorry :(") }, 2500)
 };
