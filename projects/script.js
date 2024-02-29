2b1c70dbe5f7fe47af121e25262abdd7d1fede0a
Tanvish <101194211+TanvishGG@users.noreply.github.com>
2024-02-29 11:35:51 +0000

    .

--- projects/script.js
+++ projects/script.js
@@ -1,3 +1,3 @@
 function redirect(url) {
   window.location.href = url;
-}
+};
--- script.js
+++ script.js
@@ -6,22 +6,26 @@ for (let i = 0; i < 9; i++) {
 
 const colors = ["white", "red", "blue", "green", "yellow", "orange", "skyblue", "lime", "pink"];
 function boom() {
-  for (let i = 0; i <= 17; i++) {
-    setTimeout(() => {
-      let doc = document.getElementById(`${i}`)
-      doc.textContent = doc.textContent.split("").sort((a, b) => 0.5 - Math.random()).join(" ");
-      doc.style.display = "ruby-text";
-      doc.style.position = "fixed";
-      doc.style.textDecoration = "none";
-      doc.style.whiteSpace = "normal";
-      doc.style.fontSize = "2em";
-      doc.style.letterSpacing = "0.5em";
-      doc.style.color = colors[Math.floor(Math.random() * colors.length)];
-      doc.style.textShadow = "0 0 0.5em white";
-      doc.style.top = `${Math.random() * 80}%`;
-      doc.style.transform = `rotateZ(${Math.floor(Math.random() * 360)}deg)`;
-      doc.style.transition = "all 0.4s ease";
-    }, i * 20 ?? 10);
+  function repeat() {
+    for (let i = 0; i <= 17; i++) {
+      setTimeout(() => {
+        let doc = document.getElementById(`${i}`)
+        doc.textContent = doc.textContent.split("").sort((a, b) => 0.5 - Math.random()).join(" ");
+        doc.style.display = "ruby-text";
+        doc.style.position = "fixed";
+        doc.style.textDecoration = "none";
+        doc.style.whiteSpace = "normal";
+        doc.style.fontSize = "2em";
+        doc.style.letterSpacing = "0.5em";
+        doc.style.color = colors[Math.floor(Math.random() * colors.length)];
+        doc.style.textShadow = "0 0 0.5em white";
+        doc.style.top = `${Math.random() * 80}%`;
+        doc.style.transform = `rotateZ(${Math.floor(Math.random() * 360)}deg)`;
+        doc.style.transition = "all 0.4s ease";
+      }, i * 20 ?? 10);
+    }
   }
+  repeat()
   setTimeout(() => { window.alert("ooops, i didn't mean to make the nuke button public!\nsorry :(") }, 2_000)
+  setInterval(repeat, 4_500)
 };
